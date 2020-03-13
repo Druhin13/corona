6e5cec96cd2735f25238fb9e5519ced0e1f450e4
Druhin Tarafder <druhinpro@gmail.com>
2020-03-13 19:03:26 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,16 @@
+var viewportHeight = $(window).height(); // == 100vh
+var reqheight = viewportHeight * 46/100.0; // ==46vh
+console.log(reqheight);
+
+$(document).ready(function () {
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > reqheight) {
+			//change yes to no
+			$('h1').html('Articles');
+		} else {
+			//set h1 text to yes
+			$('h1').html('Top News');
+		}
+	});
+});
