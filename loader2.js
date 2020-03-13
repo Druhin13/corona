7c74acda9c73bbb1f03a4f220a0a32dfa3b90ccf
Druhin Tarafder <druhinpro@gmail.com>
2020-03-13 21:47:06 +0530

    Add files via upload

--- loader2.js
+++ loader2.js
@@ -1,6 +1,6 @@
 $(window).on("load", function () {
 	$(".loader-wrapper").slideDown(1000).delay(2000).slideUp(1000);
 	$(".top").delay(2000).slideDown(1000);
-	$(".card").delay(2000).slideDown(1000);
+	$(".map").delay(2000).slideDown(1000);
 });
 
