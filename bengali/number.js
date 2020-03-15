384b51ec8d7ae75187547353dd516ca335e4ce95
Druhin Tarafder <druhinpro@gmail.com>
2020-03-15 23:55:17 +0530

    Add files via upload

---
+++ bengali/number.js
@@ -0,0 +1,10 @@
+var n = document.getElementsByClassName("n");  //8123
+	for (var i = 0; i < n.length; i++) {       //
+		console.log(n[i]);
+		for(var j=0; j<n[i].length;j++)
+			{
+				
+				var ch = n[i].charAt(0)
+			}
+	}
+
