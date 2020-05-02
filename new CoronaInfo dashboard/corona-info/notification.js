18de7a4a6df4f2b52cbd50f84eae56effce10902
Druhin Tarafder <druhinpro@gmail.com>
2020-05-02 20:47:22 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/notification.js
+++ new CoronaInfo dashboard/corona-info/notification.js
@@ -21,7 +21,7 @@ function notify() {
 		localStorage.setItem('notify', 1);
 
 		//bringing the card down by 10vh
-		document.getElementById("card").style.marginTop = "27.5vh";
+		document.getElementById("card").style.marginTop = "25.5vh";
 		document.getElementById("card").style.transition = "all 0.5s ease";
 	} else {
 
@@ -34,7 +34,28 @@ function notify() {
 		//bringing the card down by 10vh
 		document.getElementById("card").style.marginTop = "9vh";
 		document.getElementById("card").style.transition = "all 0.5s ease";
-
-
 	}
 }
+
+// getting the latest 2 news
+
+var api_url5 = 'https://api.covid19india.org/updatelog/log.json';
+
+async function getData5() {
+	var response5 = await fetch(api_url5);
+	var data5 = await response5.json();
+	
+	
+	console.log(data5);
+	console.log(data5.length);
+	console.log(data5[data5.length-1]);
+	console.log(data5[data5.length-1].update);
+	console.log(data5[data5.length-1].timestamp);
+	
+	
+	document.getElementById("news1-c").innerHTML = (data5[data5.length-1].update).toUpperCase();
+	
+
+}
+
+getData5();
