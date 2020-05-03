c5b4197adf3e9cc27ab50073e522949d4e1c73a0
Druhin Tarafder <druhinpro@gmail.com>
2020-05-03 10:22:07 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/india-daily.js
+++ new CoronaInfo dashboard/corona-info/india-daily.js
@@ -1,155 +1,54 @@
-const api_url4 = 'https://api.covid19india.org/data.json';
+var api_url4 = 'https://api.covid19india.org/data.json';
 
-async function getData3() {
-	const response = await fetch(api_url4);
-	const data = await response.json();
-
-
+async function getData4() {
+	var response4 = await fetch(api_url4);
+	var data = await response4.json();
 
 
+	//	console.log(data.statewise);
+	//	console.log(data.statewise.length - 1);
 
-	//date calculation
 
-	var myDate = new Date();
-	var day = myDate.getDay();
-	var month = myDate.getMonth();
-	var date = myDate.getDate();
-	if (date == 1) {
-		date = "01";
-	} else if (date == 2) {
-		date = "02";
-	} else if (date == 3) {
-		date = "03";
-	} else if (date == 4) {
-		date = "04";
-	} else if (date == 5) {
-		date = "05";
-	} else if (date == 6) {
-		date = "06";
-	} else if (date == 7) {
-		date = "07";
-	} else if (date == 8) {
-		date = "08";
-	} else if (date == 9) {
-		date = "09";
+	var state_c = data.statewise[0].deltaconfirmed;
+	var state_d = data.statewise[0].deltadeaths;
+
+	if (state_c == 0) {
+		state_c = "NO";
+	}
+	if (state_d == 0) {
+		state_d = "NO";
+	}
+
+//	console.log(data.cases_time_series[data.cases_time_series.length-1].dailyconfirmed);
+//	console.log(data.cases_time_series[data.cases_time_series.length-1].dailydeceased);
+	
+//	console.log(data.cases_time_series[data.cases_time_series.length-1].date);
+
+	var day = "TODAY - ";
+
+	if (state_c == 0 && state_d == 0) {
+		
+		day = (data.cases_time_series[data.cases_time_series.length-1].date);
+//		console.log(day);
+		
+		state_c = (data.cases_time_series[data.cases_time_series.length-1].dailyconfirmed);
+		state_d = (data.cases_time_series[data.cases_time_series.length-1].dailydeceased);
+		
 	}
 
 
-	var days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
-	var months = ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "];
-
-	var a = date + " " + months[month]; //storing today's date
-	//console.log(a);
 
 
 
+	document.getElementById("india-today").innerHTML = day + state_c + " CASES & " + state_d + " DEATHS";
 
 
-
-
-
-
-	//checking if the last data is of 'todays' or not
-
-	var l = (data.cases_time_series.length - 1);
-	var last_date = data.cases_time_series[l].date;
-	//	console.log(l);
-	console.log(data.cases_time_series[l]);
-	console.log(last_date);
-
-	if (a == last_date) {
-		console.log("It's Perfect");
-
-		//now we will show the daily data, straight from the database
-
-		document.getElementById("india-today").innerHTML = "TODAY - " + data.cases_time_series[l].dailyconfirmed + " CASES & " + data.cases_time_series[l].dailydeceased + " DEATHS";
-
-		//		document.getElementById("d_recovered").innerHTML = data.cases_time_series[l].dailyrecovered;
-
-
-
-		//if the json is ready, comment out the code below
-		//comment out the top code, and put the live value, in the down code
-
-
-
-
-		/**************************************************/
-
-		//		document.getElementById("d_total").innerHTML = 191 + 1;
-		//		document.getElementById("d_recovered").innerHTML = 10;
-		//		document.getElementById("d_deaths").innerHTML = 3 + 1;
-
-		/**************************************************/
-
-
-
-
-
-	} else {
-		console.log("We need to do more calculations");
-
-		//now we will show the daily data, straight from the database
-
-
-		//so we need to calculate  (current data - last data) 
-
-
-		//here are the calculations
-
-		//		console.log(data.statewise[0].confirmed);
-		//		console.log(data.cases_time_series[l].totalconfirmed);
-		//
-		//		console.log(data.statewise[0].recovered);
-		//		console.log(data.cases_time_series[l].totalrecovered);
-		//
-		//		console.log(data.statewise[0].deaths);
-		//		console.log(data.cases_time_series[l].totaldeceased);
-
-
-		var daily_total = data.cases_time_series[l].dailyconfirmed;
-
-		//		var daily_recovered = (data.statewise[0].recovered) - (data.cases_time_series[l].totalrecovered);
-
-		var daily_deaths = data.cases_time_series[l].dailydeceased;
-
-		var yesterday = data.cases_time_series[l].date + " - ";
-
-
-
-		//error handling - if data comes in negative (can happen if some old data is updated, and some are not)
-
-		if (daily_total < 0 || daily_total === 0) {
-			daily_total = "NO";
-		}
-		//		if (daily_recovered < 0 || daily_recovered == 0) {
-		//			daily_recovered = "NO";
-		//		}
-		if (daily_deaths < 0 || daily_deaths === 0) {
-			daily_deaths = "NO";
-		}
-		if (daily_total == NaN) {
-			daily_total = "NO";
-		}
-		//		if (daily_recovered == NaN) {
-		//			daily_recovered = "NO";
-		//		}
-		if (daily_deaths == NaN) {
-			daily_deaths = "NO";
-		}
-
-
-		document.getElementById("india-today").innerHTML = yesterday + daily_total + " CASES & " + daily_deaths + " DEATHS";
-
-
-
-	}
 
 
 }
 
-getData3();
+getData4();
 
 var interval2 = setInterval(function () {
-	getData3();
+	getData4();
 }, 60000);
--- new CoronaInfo dashboard/corona-info/state.daily.js
+++ new CoronaInfo dashboard/corona-info/state.daily.js
@@ -1,8 +1,16 @@
 var api_url6 = 'https://api.covid19india.org/data.json';
+var api_url7 = 'https://api.covid19india.org/states_daily.json';
 
 async function getData6() {
 	var response6 = await fetch(api_url6);
 	var data = await response6.json();
+	
+	var response7 = await fetch(api_url7);
+	var data2 = await response7.json();
+	
+	var last = (data2.states_daily.length)-1;
+	console.log(data2.states_daily[last-2].wb);
+	console.log(data2.states_daily[last].wb);
 
 	//	console.log(data.statewise);
 	//	console.log(data.statewise.length - 1);
@@ -10,17 +18,29 @@ async function getData6() {
 	for (i = 0; i <= data.statewise.length - 1; i++) {
 		//		console.log(data.statewise[i]);
 		if (data.statewise[i].statecode == "WB") {
+			
+			var day = "TODAY - ";
+			
 			var state_c = data.statewise[i].deltaconfirmed;
+			
 			if (state_c == 0) {
 				state_c = "NO";
 			}
 			if (state_d == 0) {
 				state_d = "NO";
 			}
+			
+			
+			
+			if(state_c == 0 && state_d == 0)
+				{
+					var day = data2.states_daily[last];
+					console.log(day);
+				}
+			
 			var state_d = data.statewise[i].deltadeaths;
 		}
 	}
-
 
 
 
