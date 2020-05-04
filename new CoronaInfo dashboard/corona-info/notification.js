da9c173791063b7ca02853eae3e3b0c7d72dfa98
Druhin Tarafder <druhinpro@gmail.com>
2020-05-04 10:41:39 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/notification.js
+++ new CoronaInfo dashboard/corona-info/notification.js
@@ -11,7 +11,7 @@ localStorage.setItem('notify', 0);
 
 
 function notify() {
-	
+
 	getData5();
 
 	if (localStorage.getItem('notify') == 0) {
@@ -43,6 +43,8 @@ function notify() {
 
 var api_url5 = 'https://api.covid19india.org/updatelog/log.json';
 
+var x = "AM";
+
 async function getData5() {
 	var response5 = await fetch(api_url5);
 	var data5 = await response5.json();
@@ -52,33 +54,48 @@ async function getData5() {
 	//	console.log(data5.length);
 	//	console.log(data5[data5.length-1]);
 	//	console.log(data5[data5.length-1].update);
-	console.log(data5[data5.length - 1].timestamp);
+	//	console.log(data5[data5.length - 1].timestamp);
 
-	console.log(Date.now());
+	//	console.log(Date.now());
 
 	function Unix_timestamp(t) {
 		var dt = new Date(t * 1000);
 		var hr = dt.getHours();
+
+		if (hr > 12) {
+			x = "PM";
+			hr = hr - 12;
+		}
+		if (hr == 12) {
+			x = "PM";
+			hr = 12;
+		}
+		if (hr < 12) {
+			x = "AM";
+		}
+		if (hr == 00) {
+			x = "AM";
+			hr = 12;
+		}
+
 		var m = "0" + dt.getMinutes();
 		var s = "0" + dt.getSeconds();
-		return hr + ' : ' + m.substr(-2) + ' : ' + s.substr(-2);
+		return hr + ' : ' + m.substr(-2);
 	}
 
-	console.log(Unix_timestamp(data5[data5.length - 1].timestamp));
-
-	var time = Unix_timestamp(data5[data5.length - 1].timestamp);
-
-
-	
+	//	console.log(Unix_timestamp(data5[data5.length - 1].timestamp));
+	var tstamp = (data5[data5.length - 1].timestamp);
+	var time = Unix_timestamp(tstamp);
 
 
+	console.log(time + " " + x);
 
 
-	document.getElementById("news1-t").innerHTML = time;
+	document.getElementById("news1-t").innerHTML = time + " " + x;
 
 
 	document.getElementById("news1-c").innerHTML = (data5[data5.length - 1].update).toUpperCase();
-
+	//	console.log(data5);
 
 }
 
