86cedc1ac725da326bbf2b151df89077f8d1c8aa
Druhin Tarafder <druhinpro@gmail.com>
2020-05-06 22:55:56 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/thanks.js
+++ new CoronaInfo dashboard/corona-info/thanks.js
@@ -1,16 +1,17 @@
-window.onload=function()  //executes when the page finishes loading
-{
-	setTimeout(func1, 10000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
-	
-};
-function func1()
-{
-	document.getElementById("thanks").style.transition = "all 10s ease";
-	document.getElementById("thanks").style.display = "block";
-	
-}
+//window.onload=function()  //executes when the page finishes loading
+//{
+//	setTimeout(func1, 10000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
+//	
+//};
+//function func1()
+//{
+//	document.getElementById("thanks").style.transition = "all 10s ease";
+//	document.getElementById("thanks").style.display = "block";
+//	
+//}
 
-function hide()
-{
-	document.getElementById("thanks").style.display = "none";
-}
+//function large()
+//{
+//	document.getElementById("donate").style.scale = "100";
+//}
+
