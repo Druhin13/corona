c7b51e89d38522f09e2fd2ba291e456937c0e320
Druhin Tarafder <druhinpro@gmail.com>
2020-05-16 18:25:36 +0530

    Add files via upload

---
+++ new CoronaInfo dashboard/corona-info/transition.js
@@ -0,0 +1,8 @@
+function trans() {
+	document.getElementById("bg").style.display = "none";
+
+//	function Redirect() {
+		window.location = "https://www.buymeacoffee.com/Druhin",1000;
+//	}
+//	setTimeout('Redirect()', 1000);
+}
