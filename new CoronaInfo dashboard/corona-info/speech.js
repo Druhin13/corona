e07fee9a20b5e6c569b3ca98e2415d73337b9aa3
Druhin Tarafder <druhinpro@gmail.com>
2020-05-20 12:31:24 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/speech.js
+++ new CoronaInfo dashboard/corona-info/speech.js
@@ -1,6 +1,14 @@
 //	responsiveVoice.enableWindowClickHook();
 //	responsiveVoice.clickEvent();
 
+
+if (responsiveVoice.voiceSupport()) {
+	responsiveVoice.speak("Welcome");
+}
+
+
+
+
 function speech() {
 
 	responsiveVoice.setDefaultVoice("UK English Male");
@@ -9,23 +17,23 @@ function speech() {
 		//  console.log("I hope you are listening");
 
 		responsiveVoice.cancel();
-		
+
 	} else {
 
 
-//		responsiveVoice.enableEstimationTimeout = false;
+		//		responsiveVoice.enableEstimationTimeout = false;
 
 		if (responsiveVoice.voiceSupport()) {
-			
-			
-//			responsiveVoice.speak(document.getElementById("india-today").textContent, "UK English Male", {pitch: 0.9});
-			
-			responsiveVoice.speak("Today at - "+document.getElementById("news1-t").textContent);
+
+
+			//			responsiveVoice.speak(document.getElementById("india-today").textContent, "UK English Male", {pitch: 0.9});
+
+			responsiveVoice.speak("Today at - " + document.getElementById("news1-t").textContent);
 			responsiveVoice.speak(document.getElementById("news1-c").textContent);
-			
-			
-			
-//			document.getElementById("speak").style.animation = "bounce-in";
+
+
+
+			//			document.getElementById("speak").style.animation = "bounce-in";
 
 
 		} else {
