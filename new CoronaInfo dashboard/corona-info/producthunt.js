e31f470db83b3839239c37a86dc213e4d7c4657d
Druhin Tarafder <druhinpro@gmail.com>
2020-05-24 20:34:04 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/producthunt.js
+++ new CoronaInfo dashboard/corona-info/producthunt.js
@@ -1,8 +1,9 @@
 function trans3() {
 	document.getElementById("all").style.opacity = "0";
 
-//	function Redirect() {
-		window.location = "https://www.producthunt.com/posts/corona-info-india",1000;
-//	}
-//	setTimeout('Redirect()', 1000);
+	//	function Redirect() {
+	window.location = "#";
+	window.location = "https://www.producthunt.com/posts/corona-info-india", 1000;
+	//	}
+	//	setTimeout('Redirect()', 1000);
 }
