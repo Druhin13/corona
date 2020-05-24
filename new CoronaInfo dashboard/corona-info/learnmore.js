d08fd2ffa7a60d0a2c2f5e3c78d7e6da84fb4073
Druhin Tarafder <druhinpro@gmail.com>
2020-05-24 20:33:47 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/learnmore.js
+++ new CoronaInfo dashboard/corona-info/learnmore.js
@@ -1,8 +1,9 @@
 function trans2() {
 	document.getElementById("all").style.opacity = "0";
 
-//	function Redirect() {
-		window.location = "https://coronavirusapp.github.io/home",1000;
-//	}
-//	setTimeout('Redirect()', 1000);
+	//	function Redirect() {
+	window.location = "#";
+	window.location = "https://coronavirusapp.github.io/home", 1000;
+	//	}
+	//	setTimeout('Redirect()', 1000);
 }
